95e2455521194ff95d6b53c7ac9cb7c50f66b0fa
Renan Cardoso <renancardoso0307@gmail.com>
2022-12-27 17:08:04 -0300

    feat: create styles global

---
+++ src/styles/global.ts
@@ -0,0 +1,24 @@
+import { createGlobalStyle } from 'styled-components';
+
+export default createGlobalStyle`
+@import url('https://fonts.googleapis.com/css?family=Roboto:400,500,600,700&display=swap');
+
+
+body, input, button {
+  font-family: 'Roboto', sans-serif;
+  font-size: 1rem;
+}
+
+button {
+    cursor: pointer;
+}
+
+body{
+    margin: 0;
+    padding: 0;
+    outline: 0;
+    box-sizing: border-box;
+    background: ${({ theme }) => theme.colors.gray100}
+}
+
+`;
