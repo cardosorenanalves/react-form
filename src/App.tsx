4ed49ff3f5ffc7b6ed58d004a5039824d8935219
Renan Cardoso <renancardoso0307@gmail.com>
2022-12-27 22:15:43 -0300

    feat: import header

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,14 @@ import { ThemeProvider } from 'styled-components';
 import { theme } from './theme';
 import Global from './styles/global';
 import { Form } from './Form';
+import { Header } from './components/Header/Header';
 
 
 function App() {
   return (
     <ThemeProvider theme={theme}>
     <Global />
+    <Header/>
     <Form />
     </ThemeProvider>
   );
