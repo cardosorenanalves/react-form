5a7295fd583a50ba0c7aab8fc43366ee8d507dd5
Renan Cardoso <renancardoso0307@gmail.com>
2023-01-18 11:37:31 -0300

    style: form indentation done

--- src/Form/styles.ts
+++ src/Form/styles.ts
@@ -1,49 +1,45 @@
 import styled from 'styled-components';
 
 export const Container = styled.div`
-max-width: 100%;
-width: 600px;
-margin: 0 auto;
-display: flex;
-flex-direction: column;
-align-items: center;
-justify-content: space-between;
-background: ${({ theme }) => theme.colors.white};
-margin-bottom: 30px;
-
+    max-width: 100%;
+    width: 600px;
+    margin: 0 auto;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: space-between;
+    background: ${({ theme }) => theme.colors.white};
+    margin-bottom: 30px;
 `;
 
 
 
 export const InputsDiv = styled.div`
-width: 100%;
-display: flex;
-flex-direction: column;
-align-items: center;
+    width: 100%;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
 
-
-.css-jh47zj-MuiButtonBase-root-MuiButton-root{
-    background-color: #ed6c02 ;
-
-    &:hover{
-        background-color: #E98010 ;
+    .css-jh47zj-MuiButtonBase-root-MuiButton-root{
+        background-color: #ed6c02 ;
+        &:hover{
+            background-color: #E98010 ;
+        }
     }
-}`;
+`;
 
 export const InputContainer = styled.div`
-flex: 1;
-display: flex; 
-flex-direction:row; 
-justify-content: center;
-width: 100%;
-margin-bottom: 15px;
-gap:1rem;
+    flex: 1;
+    display: flex; 
+    flex-direction:row; 
+    justify-content: center;
+    width: 100%;
+    margin-bottom: 15px;
+    gap:1rem;
 
-@media(max-width: 450px){
-    flex-direction: column;
-    margin-bottom:15px ;
-    width: 90%;
-}
-
-
+    @media(max-width: 450px){
+        flex-direction: column;
+        margin-bottom:15px ;
+        width: 90%;
+    }
 `;
--- src/Form/useForm.ts
+++ src/Form/useForm.ts
@@ -1,18 +1,20 @@
-import dayjs, { Dayjs } from "dayjs";
-import { toast } from "react-toastify";
-import { validateEmail } from "../utils/validateEmail";
-import { telefonemask } from "../utils/telefone";
-import { useState } from "react";
+import dayjs, { Dayjs } from 'dayjs';
+import { toast } from 'react-toastify';
+import { validateEmail } from '../utils/validateEmail';
+import { telefonemask } from '../utils/telefone';
+import { useState } from 'react';
+
+export interface IValue {
+    label: string
+}
 
 export function UseForm(){
-    const [value, setValue] = useState<Dayjs | null>(
-        dayjs(),
-      );
+    const [value, setValue] = useState<Dayjs | null>(dayjs());
     const [name, setName] = useState<string>('');  
     const [email, setEmail] = useState<string>('');
     const [tel, setTel] = useState<string>('');
-    const [bairro, setBairro] = useState<any>({});
-    const [disponibilit, setDisponibilit] = useState<any>({});
+    const [bairro, setBairro] = useState<IValue>({label: ''});
+    const [disponibilit, setDisponibilit] = useState<IValue>({label: ''});
     const [checkConfirm, setCheckConfirm] = useState<boolean>(true);
     const [nameErr, setNameErr] = useState<boolean>(false);  
     const [emailErr, setEmailErr] = useState<boolean>(false); 
@@ -21,23 +23,22 @@ export function UseForm(){
 
     const handleChange = (newValue: Dayjs | null) => {
         setValue(newValue);
-      };
+    };
  
-
     function handleSubmit(){
-        setIsLoading(true)
-        resetError()
+        setIsLoading(true);
+        resetError();
         if(!name){
-       setNameErr(true)
-       setIsLoading(false)
+            setNameErr(true);
+            setIsLoading(false);
         }
         if(!email || !validateEmail(email)){
-             setEmailErr(true)
-             setIsLoading(false)
+             setEmailErr(true);
+             setIsLoading(false);
         }
         if(!tel || telefonemask(tel)?.length as number < 17){
-             setTelErr(true)
-             setIsLoading(false)
+             setTelErr(true);
+             setIsLoading(false);
         }
 
         if(
@@ -46,38 +47,35 @@ export function UseForm(){
             && email
             && validateEmail(email)
             && name
-            ){
-           
+        ){    
             const register = {
                 nome: name,
                 email: email,
                 telefone: tel,
                 dataDeNascimento: value?.format('DD/MM/YYYY'),
-                bairro: bairro ? bairro : "Qualquer",
+                bairro: bairro ? bairro : 'Qualquer',
                 disponibilidade: disponibilit ? disponibilit : 'Qualquer',
                 contatoTel: checkConfirm ? 'Sim' : 'Não'
-            }
-            console.log(register)
+            };
+            console.log(register);
             setTimeout(()=>{
-            toast(`Cadastrado com sucesso!`, {progressStyle:{background:'#ed6c02'}});
-            setName('');
-            setEmail('');
-            setTel('');
-            setValue(dayjs());
-            setBairro({})
-            setDisponibilit({});
-            setCheckConfirm(true);
-            setIsLoading(false)
-            }, 2000)
-            
-        }
-        
+                toast('Cadastrado com sucesso!', {progressStyle:{background:'#ed6c02'}});
+                setName('');
+                setEmail('');
+                setTel('');
+                setValue(dayjs());
+                setBairro({label: ''});
+                setDisponibilit({label: ''});
+                setCheckConfirm(true);
+                setIsLoading(false);
+            }, 2000);  
+        }        
     }
 
     function resetError(){
-        setNameErr(false)
-        setEmailErr(false)
-        setTelErr(false)
+        setNameErr(false);
+        setEmailErr(false);
+        setTelErr(false);
     }
 
     return{
@@ -100,6 +98,5 @@ export function UseForm(){
         setCheckConfirm,
         handleSubmit,
         isLoading
-    }
-
+    };
 }
