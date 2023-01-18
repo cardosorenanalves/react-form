8ef1c25cb2e1682c442d382be9294f19c862fa53
Renan Cardoso <renancardoso0307@gmail.com>
2023-01-18 11:37:12 -0300

    style: app indentation done

--- src/App.tsx
+++ src/App.tsx
@@ -6,20 +6,20 @@ import { Form } from './Form';
 import { Header } from './components/Header/Header';
 import { CustomFooter } from './components/Footer/Footer';
 import { LocalizationProvider } from '@mui/x-date-pickers';
-import ptBR from 'dayjs/locale/pt-br'
-import {AdapterDayjs} from '@mui/x-date-pickers/AdapterDayjs'
+import ptBR from 'dayjs/locale/pt-br';
+import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
 import 'react-toastify/dist/ReactToastify.css';
 
 
 function App() {
   return (
     <LocalizationProvider adapterLocale={ptBR} dateAdapter={AdapterDayjs}>
-    <ThemeProvider theme={theme}>
-    <Global />
-    <Header/>
-    <Form />
-    <CustomFooter />
-    </ThemeProvider>
+      <ThemeProvider theme={theme}>
+        <Global />
+        <Header/>
+        <Form />
+        <CustomFooter />
+      </ThemeProvider>
     </LocalizationProvider>
   );
 }
