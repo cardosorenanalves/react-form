750b3d0472c4d881f092757627555d77c1287a4e
Renan Cardoso <renancardoso0307@gmail.com>
2023-01-18 10:32:04 -0300

    style: organization of footer

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,53 +1,58 @@
-import { FaFacebookF } from "react-icons/fa";
-import { GrGooglePlus, GrInstagram } from "react-icons/gr";
-import { TfiLinkedin } from "react-icons/tfi";
-import { Footer, InfoDiv, MidiaDiv } from "./styles";
+import React from 'react';
+import { FaFacebookF } from 'react-icons/fa';
+import { GrGooglePlus, GrInstagram } from 'react-icons/gr';
+import { TfiLinkedin } from 'react-icons/tfi';
+import { Footer, InfoDiv, MidiaDiv } from './styles';
 
 export function CustomFooter(){
     return(
-        <Footer
-        >
-        <InfoDiv>
-            <div>
-            <h2>Contate-nos:</h2>
-            <p>Nosso endereço:</p>
-            <p>Rua Caio Júlio César, 193 Vila Cardoso Franco - SP</p>
-            <p>E-mail: contato@imobiliaria.com.br</p>
-            <p>{`Telefone: +55 (21) 9 9999-0000`}</p>
-            </div>
-            <div >
-            <h2>Conheça também:</h2>
-            <div style={{display: 'flex', flexDirection: 'column', gap: '0.4rem'}}>
-            <MidiaDiv>
-            <GrInstagram size={15}
-            style={{marginRight: '18px'}}
-            />
-            <span>@Imobiliaria</span>
-            </MidiaDiv>
-            <MidiaDiv>
-            <FaFacebookF size={15}
-            style={{marginRight: '18px'}}
-            />
-            <span>Imobiliaria</span>
-            </MidiaDiv>
-            <MidiaDiv>
-            <TfiLinkedin size={15}
-            style={{marginRight: '18px'}}
-            />
-            <span
-            style={{marginTop: '2px'}}
-            >Imobiliaria</span>
-             </MidiaDiv>
-             <MidiaDiv>
-            <GrGooglePlus 
-            style={{marginRight: '10px'}}
-            size={21} 
-            />
-            <span>contato@Imobiliaria.com</span>
-            </MidiaDiv>
-            </div>
-            </div>
-        </InfoDiv>
+        <Footer>
+            <InfoDiv>
+                <div>
+                    <h2>Contate-nos:</h2>
+                    <p>Nosso endereço:</p>
+                    <p>Rua Caio Júlio César, 193 Vila Cardoso Franco - SP</p>
+                    <p>E-mail: contato@imobiliaria.com.br</p>
+                    <p>{'Telefone: +55 (21) 9 9999-0000'}</p>
+                </div>
+                <div >
+                    <h2>Conheça também:</h2>
+                    <div style={{display: 'flex', flexDirection: 'column', gap: '0.4rem'}}>
+                        <MidiaDiv>
+                            <GrInstagram 
+                                size={15}
+                                style={{marginRight: '18px'}}
+                            />
+                            <span>@Imobiliaria</span>
+                        </MidiaDiv>
+                        <MidiaDiv>
+                            <FaFacebookF 
+                                size={15}
+                                style={{marginRight: '18px'}}
+                            />
+                            <span>Imobiliaria</span>
+                        </MidiaDiv>
+                        <MidiaDiv>
+                            <TfiLinkedin 
+                                size={15}
+                                style={{marginRight: '18px'}}
+                            />
+                            <span 
+                            style={{marginTop: '2px'}}
+                            >
+                                Imobiliaria
+                            </span>
+                        </MidiaDiv>
+                        <MidiaDiv>
+                            <GrGooglePlus 
+                            style={{marginRight: '10px'}}
+                            size={21} 
+                            />
+                            <span>contato@Imobiliaria.com</span>
+                        </MidiaDiv>
+                    </div>
+                </div>
+            </InfoDiv>
         </Footer>
     );
 }
--- src/components/Footer/styles.ts
+++ src/components/Footer/styles.ts
@@ -1,36 +1,34 @@
-import styled from "styled-components";
+import styled from 'styled-components';
 
 export const Footer = styled.div`
-width: 100%;
-background: #ed6c02;
-color: ${({theme})=> theme.colors.white};
-border-top:1px solid;
-border-color:${({theme}) => theme.colors.gray100} ;
+    width: 100%;
+    background: #ed6c02;
+    color: ${({theme})=> theme.colors.white};
+    border-top:1px solid;
+    border-color:${({theme}) => theme.colors.gray100};
 `;
 
-
 export const InfoDiv = styled.div`
-font-size: 12px;
-line-height: 10px;
-display: flex;
-flex-direction: row;
-align-items: flex-start;
-justify-content: center;
-text-align:center;
-gap: 5rem;
+    font-size: 12px;
+    line-height: 10px;
+    display: flex;
+    flex-direction: row;
+    align-items: flex-start;
+    justify-content: center;
+    text-align:center;
+    gap: 5rem;
 
-@media(max-width: 450px){
-    gap: 0;
-    flex-direction: column;
-    align-items: center;
-justify-content: center;
-margin-bottom: 15px;
-}
-
+    @media(max-width: 450px){
+        gap: 0;
+        flex-direction: column;
+        align-items: center;
+        justify-content: center;
+        margin-bottom: 15px;
+    }
 `;
 
 export const MidiaDiv = styled.div`
-margin-left: 8px;
-display: flex;
-align-items: center;
-`
+    margin-left: 8px;
+    display: flex;
+    align-items: center;
+`;
